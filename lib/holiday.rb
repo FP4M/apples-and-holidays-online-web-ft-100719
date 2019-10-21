0e7b7ab3352ea5bc8a2b908f73e8a1207fcb95ba
Prince Charles <princecharles35@yahoo.com>
2019-10-21 16:53:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,3 +19,19 @@ def second_supply_for_fourth_of_july(holiday_supplies)
   holiday_supplies[:summer][:fourth_of_july][1]
 end
 
+holiday_hash.each do |season, data|
+    puts "#{season.to_s.capitalize!}:"
+    data.each do |holiday, supply|
+      array = holiday.to_s.split("_")
+      final_holiday = []
+        array.each do |x|
+          final_holiday << x.capitalize!
+        end
+      holiday = final_holiday.join(" ")
+
+      supply = supply.join(", ")
+      puts "  #{holiday}: #{supply}"
+
+    end
+  end
+
